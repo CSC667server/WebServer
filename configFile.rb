85dda3ea77b8bf15b16bf595a2a8d80cce4b8bdd
Guan <guanlin92@yahoo.com>
2016-02-26 22:59:32 -0800

    implemented configFile Object

--- configFile.rb
+++ configFile.rb
@@ -1,13 +1,29 @@
 class ConfigFile 
 
-	attr_accessor :line
+	attr_accessor :lines, :directives
 
-	def intialize()
-
+	def initialize(config)
+		@lines = config
+		@directives = {}
 	end
 
+	# filter and store config to @directives
 	def load()
+		@lines.each do |line|
+			if line.length != 0 and line[0] != "#" and line[0] != "\n"
+				directive, value = line.split(" ", 2)
+				@directives.store(directive, value.chomp)
+			end
+		end
 
+		return @directives
 	end 
+
+	# display @directive to console for testing
+	def display()
+		@directives.each do |key, value|
+			puts "#{key} => #{value}"
+		end
+	end
 
 end
