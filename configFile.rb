e2419b228b528742b937774d29134a5090b24394
Guan <guanlin92@yahoo.com>
2016-02-28 14:37:43 -0800

    change load() to process_lines()

--- configFile.rb
+++ configFile.rb
@@ -2,17 +2,18 @@ class ConfigFile
 
 	attr_accessor :lines, :directives
 
-	def initialize(config)
-		@lines = config
+	def initialize(lines)
+		@lines = lines
 		@directives = {}
 	end
 
-	# filter and store config to @directives
-	def load()
+	# filter and store lines to @directives
+	# returns @directives hash
+	def process_lines()
 		@lines.each do |line|
 			if line.length != 0 and line[0] != "#" and line[0] != "\n"
-				directive, value = line.split(" ", 2)
-				@directives.store(directive, value.chomp)
+				directive, argument = line.split(" ", 2)
+				@directives.store(directive, argument.chomp)
 			end
 		end
 
@@ -20,10 +21,12 @@ class ConfigFile
 	end 
 
 	# display @directive to console for testing
-	def display()
+	def show_config()
 		@directives.each do |key, value|
 			puts "#{key} => #{value}"
 		end
+
+		return self
 	end
 
 end
