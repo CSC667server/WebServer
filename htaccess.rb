6ca2c9bd861afa76e61341a12512e6c54acaaaa1
Guan <guanlin92@yahoo.com>
2016-02-28 18:19:48 -0800

    added htaccess file and Htaccess class

---
+++ htaccess.rb
@@ -0,0 +1,16 @@
+require './configFile.rb'
+
+class Htaccess < ConfigFile
+
+	attr_accessor :config
+
+	# need to implement
+	def initialize(lines)
+
+	end
+
+	# need to implement
+	def auth_user_file
+
+	end
+end
